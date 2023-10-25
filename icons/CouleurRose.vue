166984fce5539c1a17d835504d60d79e0f028f4d
Kiruake <ayrton.chaves@edu.univ-fcomte.fr>
2023-10-25 23:15:49 +0200

    Test Deploiement Tavue

---
+++ icons/CouleurRose.vue
@@ -0,0 +1,5 @@
+<template>
+<svg xmlns="http://www.w3.org/2000/svg" width="46" height="46" viewBox="0 0 46 46" fill="none">
+    <circle cx="23" cy="23" r="23" fill="#F39A91"/>
+  </svg>
+</template>
